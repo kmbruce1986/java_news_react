9fe807fd815eb66e7246c5f0e57b5bd05e297b3a
anne-other <anne.c.ince@gmail.com>
2018-10-20 19:08:02 +0100

    setting up new journalist form

--- src/containers/journalists/JournalistContainer.js
+++ src/containers/journalists/JournalistContainer.js
@@ -11,19 +11,64 @@ class JournalistContainer extends Component {
   }
 
   componentDidMount(){
-    fetch(this.url)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      this.setState({journalist: data})
+    if (this.url != null) {
+      fetch(this.url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        this.setState({journalist: data})
+      })
+    }
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    let requestType;
+    if (JournalistContainer.url != null) {
+      requestType = 'PUT'
+    } else {
+      requestType = 'POST'
+    }
+    fetch("/journalists",{
+      method: requestType,
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        "firstName": event.target.firstName.value,
+        "lastName": event.target.lastName.value,
+        "jobTitle": event.target.jobTitle.value,
+        "portraitURL": event.target.portraitURL.value,
+        "bio": event.target.bio.value,
+        "twitterHandle": event.target.twitterHandle.value
+      })
+    }).then(() => {
+      window.location = "/";
     })
   }
 
 
   render(){
-    return (
-      <div>JournalistContainer</div>
-    )
+    if (!this.state.journalist) {
+      return (
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" placeholder="First Name" name="firstName"/>
+            <input type="text" placeholder="Last Name" name="lastName"/>
+            <input type="text" placeholder="Job Title" name="jobTitle"/>
+            <input type="text" placeholder="Portrait Link" name="portraitURL"/>
+            <textarea placeholder="Bio" name="bio" cols="30" rows="10"></textarea>
+            <input type="text" placeholder="Twitter Handle" name="twitterHandle"/>
+            <button type="submit">Save</button>
+          </form>
+        </div>
+      )
+    }
+    else {
+      return (
+        <div>
+          edit
+        </div>
+      )
+    }
   }
 }
 
