52810139dcca685567c490552fae8f02a9140c2b
anne-other <anne.c.ince@gmail.com>
2018-10-23 11:00:54 +0100

    adding link to the article head

--- src/components/heads/ArticleHead.js
+++ src/components/heads/ArticleHead.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const ArticleHead =  (props) => {
 
-
+  let url = "/article/" + props.article.id;
   return (
-    <div>{props.article.headline}</div>
+    <div><a href={url}>{props.article.headline}</a></div>
   )
 
 
