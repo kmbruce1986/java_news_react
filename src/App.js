a2f32a6113ed3cf7f867b794115bc0f91af953e9
Andrew Craigie <andrew@craigie.co>
2018-10-23 13:41:56 +0100

    Changes isAdmin default to false

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      isAdmin: true,
+      isAdmin: false,
       categories: null,
       journalists: null,
       imageStore: "http://localhost:8080/images/"
