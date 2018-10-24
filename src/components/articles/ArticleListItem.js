82ed855709485bf99ced8d64eb94c8e14f01188f
kmbruce1986 <kmbruce1986@gmail.com>
2018-10-24 12:06:04 +0100

    adds flex for grid on front page

--- src/components/articles/ArticleListItem.js
+++ src/components/articles/ArticleListItem.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const ArticleListItem = (props) => {
   return(
-    <div className="individual-article">{props.article.headline}</div>
+    <div className="individual-article">
+      <img className="thumbnail-image" src={"http://localhost:8080/images/"+ props.article.thumbnailImage} alt=""/><h2>{props.article.headline} </h2>
+    </div>
   )
 }
 
