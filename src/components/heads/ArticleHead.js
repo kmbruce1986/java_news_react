67149fe1c1ef76d9989141a57811482e36a9063f
kmbruce1986 <kmbruce1986@gmail.com>
2018-10-24 14:16:42 +0100

    adds h tages to article head

--- src/components/heads/ArticleHead.js
+++ src/components/heads/ArticleHead.js
@@ -4,7 +4,7 @@ const ArticleHead =  (props) => {
 
   let url = "/article/" + props.article.id;
   return (
-    <div><a href={url}>{props.article.headline}</a></div>
+    <div><a href={url}><h1>{props.article.headline}</h1> <br/> <h2>{props.article.subline} </h2> <img src={"http://localhost:8080/images/"+ props.article.thumbnailImage} alt=""/> </a></div>
   )
 
 
