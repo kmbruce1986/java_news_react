888f197a428261752214a042a1c9899aa20c23b8
kmbruce1986 <kmbruce1986@gmail.com>
2018-10-24 19:17:50 +0100

    fixes flex issue with content head

--- src/components/heads/CategoryHead.js
+++ src/components/heads/CategoryHead.js
@@ -5,7 +5,7 @@ const CategoryHead =  (props) => {
   if(props.category){
     return (
       <div className="content-head">
-        <h1>{props.category.title}</h1>
+        <h1>&emsp;&emsp;{props.category.title}</h1>&emsp;&emsp;&emsp;&nbsp;
           <img className="category-img" src={"/images/" + props.category.title + ".jpg"} alt=""/>
       </div>
     )
