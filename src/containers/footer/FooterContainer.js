98665ce001eb338135f24c01f6effefd22536c84
kmbruce1986 <kmbruce1986@gmail.com>
2018-10-25 09:18:27 +0100

    log fix

--- src/containers/footer/FooterContainer.js
+++ src/containers/footer/FooterContainer.js
@@ -4,7 +4,7 @@ const FooterContainer = (props) => {
 
   return (
     <footer className="footer">
-      <img className="logo-footer" src="SGNN.png" alt="logo" height="50"/>
+      <img className="logo-footer" src="/SGNN.png" alt="logo" height="50"/>
       <div className="contact-details">SGNN<br/> 77 Renfew Street<br/> Glasgow<br/> G1 AKA</div>
     </footer>
   )
